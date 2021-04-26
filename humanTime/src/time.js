d0a04025aa0744b8bbffc29f37b778686bb9bc2a
Charlie Fischer <charlie.a.fischer@gmail.com>
2021-04-26 20:52:47 +0100

    passed second test

--- humanTime/src/time.js
+++ humanTime/src/time.js
@@ -1,5 +1,8 @@
-time = () => {
-  return '00:00:01'
+time = seconds => {
+  if(seconds == 1){
+    return '00:00:01'
+  }
+  return '00:01:00'
 }
 
 module.exports = time
--- humanTime/test/time.test.js
+++ humanTime/test/time.test.js
@@ -5,3 +5,9 @@ describe('seconds', () => {
     expect(time(1)).toEqual('00:00:01')
   })
 })
+
+describe('minutes', () => {
+  test('60 returns 00:01:00', () => {
+    expect(time(60)).toEqual('00:01:00')
+  })
+})
