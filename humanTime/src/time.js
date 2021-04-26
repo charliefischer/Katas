d4163d3b6f08be44b0e0f19c1a51a06f542fe08d
Charlie Fischer <charlie.a.fischer@gmail.com>
2021-04-26 21:30:26 +0100

    refactored

--- humanTime/src/time.js
+++ humanTime/src/time.js
@@ -1,25 +1,7 @@
 time = seconds => {
-  let s = (0).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
-  let m = 0
-  let h = 0
-
-  for(let i = 0; i < seconds; i++){
-    if(s >= 59){
-      s = 0
-      if(m >= 59) {
-        m = 0
-        h++
-      } else {
-        m++
-      }
-    } else {
-      s++
-    }
-  }
-  // (6).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
-  let secs = (s).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
-  let mins = (m).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
-  let hrs = (h).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
+  let hrs = parseInt((seconds / 3600)).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
+  let mins = parseInt(((seconds / 60) % 60)).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
+  let secs = parseInt((seconds % 60)).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
 
   return `${hrs}:${mins}:${secs}`
 }
