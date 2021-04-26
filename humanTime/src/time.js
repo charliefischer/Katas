876bdb13625de4eec9f43dd36ab2831d9f76c306
Charlie Fischer <charlie.a.fischer@gmail.com>
2021-04-26 21:19:04 +0100

    pass all tests

--- humanTime/src/time.js
+++ humanTime/src/time.js
@@ -1,5 +1,5 @@
 time = seconds => {
-  let s = 0
+  let s = (0).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
   let m = 0
   let h = 0
 
@@ -16,7 +16,12 @@ time = seconds => {
       s++
     }
   }
-  return `0${h}:0${m}:0${s}`
+  // (6).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
+  let secs = (s).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
+  let mins = (m).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
+  let hrs = (h).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
+
+  return `${hrs}:${mins}:${secs}`
 }
 
 module.exports = time
--- humanTime/test/time.test.js
+++ humanTime/test/time.test.js
@@ -4,6 +4,9 @@ describe('seconds', () => {
   test('1 second returns 00:00:01', () => {
     expect(time(1)).toEqual('00:00:01')
   })
+  test('2 seconds returns 00:00:02', () => {
+    expect(time(2)).toEqual('00:00:02')
+  })
 })
 
 describe('minutes', () => {
@@ -15,5 +18,11 @@ describe('minutes', () => {
 describe('hours', () => {
   test('3600 returns 01:00:00', () => {
     expect(time(3600)).toEqual('01:00:00')
+  })
+  test('3601 returns 01:00:01', () => {
+    expect(time(3601)).toEqual('01:00:01')
+  })
+  test('3611 returns 01:00:11', () => {
+    expect(time(3611)).toEqual('01:00:11')
   })
 })
